7a7a0b02dc2e35d3f3531fa05125335f25b004b5
Frederick <fredmtonline@gmail.com>
2025-01-07 21:41:28 +0400

    test ssr.tsx change for forge deployment

--- resources/js/ssr.tsx
+++ resources/js/ssr.tsx
@@ -1,3 +1,12 @@
+import { createInertiaApp } from '@inertiajs/react';
+import createServer from '@inertiajs/react/server';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import ReactDOMServer from 'react-dom/server';
+import { RouteName } from 'ziggy-js';
+import { route } from '../../vendor/tightenco/ziggy';
+import {MantineProvider} from "@mantine/core";
+import theme from "@/styles/theme";
+import {Notifications} from "@mantine/notifications";
 import "../css/app.css";
 import "../css/preflight.css";
 import "./bootstrap";
@@ -6,17 +15,8 @@ import "@mantine/carousel/styles.css";
 import "@mantine/notifications/styles.css";
 import "@mantine/dates/styles.css";
 import "@mantine/dropzone/styles.css";
-import { createInertiaApp } from "@inertiajs/react";
-import createServer from "@inertiajs/react/server";
-import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
-import ReactDOMServer from "react-dom/server";
-import { route, type RouteName } from 'ziggy-js';
-import { Ziggy as ziggy } from '@/ziggy';
-import { MantineProvider } from "@mantine/core";
-import theme from "./styles/theme";
-import { Notifications } from "@mantine/notifications";
 
-const appName = import.meta.env.VITE_APP_NAME || "Laravel";
+const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createServer((page) =>
     createInertiaApp({
@@ -26,22 +26,24 @@ createServer((page) =>
         resolve: (name) =>
             resolvePageComponent(
                 `./Pages/${name}.tsx`,
-                import.meta.glob("./Pages/**/*.tsx")
+                import.meta.glob('./Pages/**/*.tsx'),
             ),
         setup: ({ App, props }) => {
+            /* eslint-disable */
             // @ts-expect-error
             global.route<RouteName> = (name, params, absolute) =>
-                // @ts-expect-error
                 route(name, params as any, absolute, {
-                    ...ziggy,
                     // @ts-expect-error
-                    location: new URL(page.props.ziggy.location)
+                    ...page.props.ziggy,
+                    // @ts-expect-error
+                    location: new URL(page.props.ziggy.location),
                 });
+            /* eslint-enable */
 
-            return <MantineProvider theme={theme} defaultColorScheme="dark">
+            return  <MantineProvider theme={theme} defaultColorScheme="dark">
                 <Notifications />
                 <App {...props} />
             </MantineProvider>;
-        }
-    })
+        },
+    }),
 );
