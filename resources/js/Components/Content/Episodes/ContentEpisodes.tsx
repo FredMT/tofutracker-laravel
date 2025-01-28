269a84e013fe959a889ba2b11099e0ad04832e53
Frederick <125679913+FredMT@users.noreply.github.com>
2025-01-28 17:55:00 +0400

    Sort tv content episodes by episode number

--- resources/js/Components/Content/Episodes/ContentEpisodes.tsx
+++ resources/js/Components/Content/Episodes/ContentEpisodes.tsx
@@ -1,22 +1,24 @@
 import ContentEpisodeCard from "@/Components/Content/Episodes/ContentEpisodeCard";
-import {TvSeason} from "@/types";
-import {Divider, Stack, Title} from "@mantine/core";
-import {usePage} from "@inertiajs/react";
+import { TvSeason } from "@/types";
+import { Divider, Stack, Title } from "@mantine/core";
+import { usePage } from "@inertiajs/react";
 
 function ContentEpisodes() {
-    const {data} = usePage<{data: TvSeason}>().props
+    const { data } = usePage<{ data: TvSeason }>().props;
     return (
         <Stack mb={24}>
             <Divider my={16} />
             <Title order={3}>Episodes</Title>
-            {data.episodes.map((episode) => (
-                <ContentEpisodeCard
-                    episode={episode}
-                    key={episode.id}
-                    imageSource="tmdb"
-                    type="tv"
-                />
-            ))}
+            {[...data.episodes]
+                .sort((a, b) => a.episode_number - b.episode_number)
+                .map((episode) => (
+                    <ContentEpisodeCard
+                        episode={episode}
+                        key={episode.id}
+                        imageSource="tmdb"
+                        type="tv"
+                    />
+                ))}
         </Stack>
     );
 }
