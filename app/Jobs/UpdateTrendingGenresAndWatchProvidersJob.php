6371bd9f2516f1dea934bc67008d1b93931b9fda
Frederick <fredmtonline@gmail.com>
2025-03-10 13:09:54 +0530

    remove ShouldBeUnique from UpdateTrendingGenresAndWatchProvidersJob

--- app/Jobs/UpdateTrendingGenresAndWatchProvidersJob.php
+++ app/Jobs/UpdateTrendingGenresAndWatchProvidersJob.php
@@ -4,13 +4,12 @@ namespace App\Jobs;
 
 use App\Actions\Trending\GetTrendingGenresAndWatchProvidersAction;
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldBeUnique;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 
-class UpdateTrendingGenresAndWatchProvidersJob implements ShouldQueue, ShouldBeUnique
+class UpdateTrendingGenresAndWatchProvidersJob implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
