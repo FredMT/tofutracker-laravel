b32aa3084dc5e60ee162beecc2660666eae9c7bd
Frederick <fredmtonline@gmail.com>
2025-03-25 15:52:28 +0530

    Remove unused code in theme.ts

--- resources/js/styles/theme.ts
+++ resources/js/styles/theme.ts
@@ -36,49 +36,15 @@ const theme = createTheme({
     },
     fontFamily: "Inter, sans-serif",
     headings: {
-        // Use default theme if you want to provide default Mantine fonts as a fallback
         fontFamily: `Inter, sans-serif`,
     },
     colors: {
-        // zinc: zincColors,
-        // slate: slateColors,
-        // stone: stoneColors,
-        // gray: grayColors,
-        // neutral: neutralColors,
-        // red: redColors,
-        // rose: roseColors,
-        // orange: orangeColors,
-        // green: greenColors,
-        // blue: blueColors,
-        // yellow: yellowColors,
-        // violet: violetColors,
-        // primary: zincColors,
-        // secondary: zincColors, // secondary will be dynamically changed baesd on the primary color
-        // dark: zincColors, // dark will always be secondary color
-        // error: redColors,
-        // warning: amberColors,
-        // success: greenColors,
         danger: virtualColor({
             name: "danger",
             light: "red",
             dark: "red",
         }),
     },
-    // scale: 1,
-    // primaryColor: "primary",
-    // primaryShade: { light: 8, dark: 0 },
-    // autoContrast: true,
-    // luminanceThreshold: 0.3,
-    // fontFamily: "Inter, sans-serif",
-    // fontFamilyMonospace: "Monaco, Courier, monospace",
-    // radius: {
-    //     xs: rem("6px"),
-    //     sm: rem("8px"),
-    //     md: rem("12px"),
-    //     lg: rem("16px"),
-    //     xl: rem("24px"),
-    // },
-    // defaultRadius: "sm",
     spacing: {
         "3xs": rem("4px"),
         "2xs": rem("8px"),
@@ -90,113 +56,6 @@ const theme = createTheme({
         "2xl": rem("28px"),
         "3xl": rem("32px"),
     },
-    // fontSizes: {
-    //     xs: rem("12px"),
-    //     sm: rem("14px"),
-    //     md: rem("16px"),
-    //     lg: rem("18px"),
-    //     xl: rem("20px"),
-    //     "2xl": rem("24px"),
-    //     "3xl": rem("30px"),
-    //     "4xl": rem("36px"),
-    //     "5xl": rem("48px"),
-    // },
-    // lineHeights: {
-    //     xs: rem("18px"),
-    //     sm: rem("20px"),
-    //     md: rem("24px"),
-    //     lg: rem("28px"),
-    // },
-    // headings: {
-    //     fontFamily: "Greycliff CF, sans-serif",
-    //     sizes: {
-    //         h1: {
-    //             fontSize: rem("36px"),
-    //             lineHeight: rem("44px"),
-    //             fontWeight: "600",
-    //         },
-    //         h2: {
-    //             fontSize: rem("30px"),
-    //             lineHeight: rem("38px"),
-    //             fontWeight: "600",
-    //         },
-    //         h3: {
-    //             fontSize: rem("24px"),
-    //             lineHeight: rem("32px"),
-    //             fontWeight: "600",
-    //         },
-    //         h4: {
-    //             fontSize: rem("20px"),
-    //             lineHeight: rem("30px"),
-    //             fontWeight: "600",
-    //         },
-    //     },
-    // },
-    // shadows: {
-    //     xs: "0 1px 2px rgba(0, 0, 0, 0.05)",
-    //     sm: "0 1px 3px rgba(0, 0, 0, 0.1), 0 1px 2px rgba(0, 0, 0, 0.06)",
-    //     md: "0 4px 6px rgba(0, 0, 0, 0.1), 0 2px 4px rgba(0, 0, 0, 0.06)",
-    //     lg: "0 10px 15px rgba(0, 0, 0, 0.1), 0 4px 6px rgba(0, 0, 0, 0.05)",
-    //     xl: "0 20px 25px rgba(0, 0, 0, 0.1), 0 10px 10px rgba(0, 0, 0, 0.04)",
-    //     xxl: "0 25px 50px rgba(0, 0, 0, 0.25)",
-    // },
-    // cursorType: "pointer",
-    // other: {
-    //     style: "shadcn",
-    // },
-    // variantColorResolver: (component) => {
-    //     const defaultResolvedColors = defaultVariantColorsResolver(component);
-    //     if (component.variant === "default") {
-    //         return {
-    //             ...defaultResolvedColors,
-    //             background: "var(--mantine-color-default)",
-    //             border: "1px solid var(--mantine-color-default-border)",
-    //             hover: "var(--mantine-color-default-hover)",
-    //         };
-    //     }
-    //     if (component.variant === "filled") {
-    //         return {
-    //             ...defaultResolvedColors,
-    //             background: "var(--mantine-primary-color-filled)",
-    //             hover: "var(--mantine-primary-color-filled-hover)",
-    //             color: "var(--mantine-primary-color-contrast)",
-    //         };
-    //     }
-    //     if (component.variant === "light") {
-    //         return {
-    //             ...defaultResolvedColors,
-    //             background: "var(--mantine-primary-color-light)",
-    //             hover: "var(--mantine-primary-color-light-hover)",
-    //             color: "var(--mantine-primary-color-light-color)",
-    //         };
-    //     }
-    //     if (component.variant === "outline") {
-    //         return {
-    //             ...defaultResolvedColors,
-    //             background: "var(--mantine-color-default)",
-    //             border: "1px solid var(--mantine-color-default-border)",
-    //             hover: "var(--mantine-color-default-hover)",
-    //             color: "var(--mantine-color-default-color)",
-    //         };
-    //     }
-    //     if (component.variant === "subtle") {
-    //         return {
-    //             ...defaultResolvedColors,
-    //             background: "transparent",
-    //             hover: "var(--mantine-color-default-hover)",
-    //             color: "var(--mantine-color-default-color)",
-    //         };
-    //     }
-    //     if (component.variant === "danger") {
-    //         return {
-    //             ...defaultResolvedColors,
-    //             background: "var(--mantine-color-danger-6)",
-    //             hover: "var(--mantine-color-danger-7)",
-    //             color: "var(--mantine-color-white)",
-    //         };
-    //     }
-    //     return defaultResolvedColors;
-    // },
     components: {
         Container: Container.extend({
             vars: (_, { size, fluid }) => ({
