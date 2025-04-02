7067e20cc4e46631e7f9e1ff8a37e36633dcdd30
Frederick <fredmtonline@gmail.com>
2025-04-02 20:33:19 +0530

    use popcorn logo

--- resources/js/Layouts/AuthenticatedLayout/components/Logo.tsx
+++ resources/js/Layouts/AuthenticatedLayout/components/Logo.tsx
@@ -1,14 +1,18 @@
-import { Link } from "@inertiajs/react";
-import { Image, Title } from "@mantine/core";
+import { Link } from '@inertiajs/react';
+import { Image, Title } from '@mantine/core';
 
 export default function Logo() {
-    return (
-        <div className="flex">
-            <div className="flex shrink-0 items-center">
-                <Link href="/">
-                    <Image src="/tofutrackerlogocamera.svg" h={60} w={60} />
-                </Link>
-            </div>
-        </div>
-    );
+	return (
+		<div className='flex'>
+			<div className='flex shrink-0 items-center'>
+				<Link href='/'>
+					<Image
+						src='/tofutrackerlogopopcorn.svg'
+						h={60}
+						w={60}
+					/>
+				</Link>
+			</div>
+		</div>
+	);
 }
