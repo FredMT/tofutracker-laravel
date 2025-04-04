da5a5ebb4cdf700d1e55e70f403721e49496c50d
Frederick <fredmtonline@gmail.com>
2025-04-04 14:35:21 +0530

    Add admin icon to NavbarRight for superuser user

--- resources/js/Layouts/AuthenticatedLayout/components/UserMenu/NavbarRight.tsx
+++ resources/js/Layouts/AuthenticatedLayout/components/UserMenu/NavbarRight.tsx
@@ -1,25 +1,38 @@
-import NotificationBellMenu from "@/Components/Notifications/components/NotificationBellMenu";
-import ThemeButton from "@/Components/ThemeButton";
-import AuthButtons from "@/Layouts/AuthenticatedLayout/components/UserMenu/AuthButtons";
-import UserDropdown from "@/Layouts/AuthenticatedLayout/components/UserMenu/UserDropdown";
-import { PageProps } from "@/types";
-import { usePage } from "@inertiajs/react";
-import { Group } from "@mantine/core";
-import ScheduleLinkActionIcon from "./ScheduleLinkActionIcon";
+import NotificationBellMenu from '@/Components/Notifications/components/NotificationBellMenu';
+import ThemeButton from '@/Components/ThemeButton';
+import AuthButtons from '@/Layouts/AuthenticatedLayout/components/UserMenu/AuthButtons';
+import UserDropdown from '@/Layouts/AuthenticatedLayout/components/UserMenu/UserDropdown';
+import { PageProps } from '@/types';
+import { usePage } from '@inertiajs/react';
+import { ActionIcon, Button, Group } from '@mantine/core';
+import ScheduleLinkActionIcon from './ScheduleLinkActionIcon';
+import { UserRoundCog } from 'lucide-react';
 
 export default function NavbarRight() {
-    const {
-        auth: { user },
-    } = usePage<PageProps>().props;
-    const component = usePage().component;
-    return (
-        <div className="hidden sm:ms-6 sm:flex sm:items-center">
-            <Group gap={16}>
-                {component !== "Schedule" && <ScheduleLinkActionIcon />}
-                <ThemeButton />
-                {user && <NotificationBellMenu />}
-                {user ? <UserDropdown /> : <AuthButtons />}
-            </Group>
-        </div>
-    );
+	const {
+		auth: { user },
+		permissions,
+	} = usePage<PageProps>().props;
+	const component = usePage().component;
+	return (
+		<div className='hidden sm:ms-6 sm:flex sm:items-center'>
+			<Group gap={16}>
+				{permissions.is_superuser && (
+					<ActionIcon
+						variant='outline'
+						size='lg'
+						component='a'
+						href={route('admin.show')}
+						target='_blank'
+					>
+						<UserRoundCog />
+					</ActionIcon>
+				)}
+				{component !== 'Schedule' && <ScheduleLinkActionIcon />}
+				<ThemeButton />
+				{user && <NotificationBellMenu />}
+				{user ? <UserDropdown /> : <AuthButtons />}
+			</Group>
+		</div>
+	);
 }
