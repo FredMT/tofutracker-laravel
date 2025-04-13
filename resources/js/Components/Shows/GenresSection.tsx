2b983cde504066b619a1c3695a12d252b6a6395f
Frederick <fredmtonline@gmail.com>
2025-04-13 14:05:57 +0530

    Remove console.log

--- resources/js/Components/Shows/GenresSection.tsx
+++ resources/js/Components/Shows/GenresSection.tsx
@@ -1,19 +1,19 @@
-import { useShowsPageGenres } from "@/propsHooks/useShowsPageGenres";
-import { Deferred } from "@inertiajs/react";
-import { GenresSkeleton } from "@/Components/Shows/components/GenresSkeleton";
-import { GenresCarousels } from "@/Components/Shows/components/GenresCarousels";
+import { useShowsPageGenres } from '@/propsHooks/useShowsPageGenres';
+import { Deferred } from '@inertiajs/react';
+import { GenresSkeleton } from '@/Components/Shows/components/GenresSkeleton';
+import { GenresCarousels } from '@/Components/Shows/components/GenresCarousels';
 
-export const GenresSection = () => {
-    const genres = useShowsPageGenres();
+export function GenresSection() {
+	const genres = useShowsPageGenres();
 
-    return (
-        <div className="space-y-4">
-            <Deferred
-                data="genres"
-                fallback={<GenresSkeleton />}
-            >
-                <GenresCarousels genresProp={genres} />
-            </Deferred>
-        </div>
-    );
-};
+	return (
+		<div className='space-y-4'>
+			<Deferred
+				data='genres'
+				fallback={<GenresSkeleton />}
+			>
+				<GenresCarousels genresProp={genres} />
+			</Deferred>
+		</div>
+	);
+}
